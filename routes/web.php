95ae028a068a311bea46f4943c61e8aad5bb748a
rifdah <rifdahfeb@gmail.com>
2023-12-27 16:17:27 -0800

    memebuat cotroler mahasiswacontroller menggunakan controller resource dengan membuat halama mahasiswa da creatmahasiswa

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,4 @@ use Illuminate\Support\Facades\Route;
 //Route::get('profile', function () {
  //   return view('profile');
 //});
-route::get('mahasiswa',[mahasiswaController::class,'index']);
+route::resource('mahasiswa',mahasiswaController::class);
